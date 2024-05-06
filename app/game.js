f3da65eea01d47bd2ad995ff85024c512f330d69
Txpts_Jdev <141035470+ThirapatS@users.noreply.github.com>
2024-05-07 03:37:36 +0700

    Add files via upload

---
+++ app/game.js
@@ -0,0 +1,42 @@
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+const imgSrc = document.getElementById("imgSrc");
+const textLabel = document.getElementById("text");
+let point = 0;
+
+noBtn.addEventListener("click", () => {
+    if (point == 0) {
+        imgSrc.src = "img/fuck-fuck-you.gif";
+        textLabel.innerText = "เอาใหม่";
+    }
+    switch (point) {
+        case 1:
+            textLabel.innerText = "ไอสัสกดใหม่";
+            break;
+        case 2:
+            textLabel.innerText = "ไม่เอาๆกดใหม่";
+            break;
+        case 3:
+            textLabel.innerText = "เตือนละนะ";
+            break;
+        case 4:
+            imgSrc.src = "img/happy.gif";
+            textLabel.innerText = "เป็นเพื่อนกับกูมั้ย";
+            noBtn.style.display = 'none';
+            break;
+    }
+    point += 1;
+});
+
+yesBtn.addEventListener("click", () => {
+    console.log(point);
+    if (point == 0 ) {
+        imgSrc.src = "img/milk-and-mocha-bear.gif";  
+        textLabel.innerText = "ใจง่ายว่ะ";      
+    } 
+
+    if (point > 0) {
+        imgSrc.src = "img/fuck-fuck-you.gif";
+        textLabel.innerText = "ทำไมไม่กดปุ่มนี่ปุ่มแรก"
+    }
+});
